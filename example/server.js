3b0ef0c2c0cab33bfb380c9ccbed2e9187679395
Martin Cerny <cerny.mrtn@gmail.com>
2023-04-04 21:21:14 +0200

    Remove example

--- example/server.js
+++
@@ -1,5 +0,0 @@
-var pseudolocale = require('../index'), // require('pseudolocale') if installed with npm
-  str = 'Thank you for using %pseudolocale.js%.';
-
-console.log('Before: ', str);
-console.log('After : ', pseudolocale(str));
